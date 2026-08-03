0ca60a0da0dc0c87d1ee90ce8442dbe6e35fd7dd
japhetcheremo-cloud <japhetcheremo@gmail.com>
2026-08-03 15:03:38 +0300

    Fix production database configuration

--- cool/settings.py
+++ cool/settings.py
@@ -113,12 +113,31 @@ WSGI_APPLICATION = "cool.wsgi.application"
 # DATABASE
 # --------------------------------------------------
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+# --------------------------------------------------
+# DATABASE
+# --------------------------------------------------
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # --------------------------------------------------
